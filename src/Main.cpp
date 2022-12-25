2fb7d34bbdc2bf753a846338445ae053d8312764
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-25 17:30:26 -0500

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,43 @@
+#include <chrono>
+#include <functional>
+#include <thread>
+#include <stdexcept>
+#include <iostream>
+
 #include "DXRApplication.h"
+
+struct ParamSTRUCT
+{
+    MSG msg = { 0 };
+    DXRApplication app;
+};
+
+void set_interval(
+    std::function<void(ParamSTRUCT&)> callback,
+    ParamSTRUCT& ParamStruct,
+    std::chrono::milliseconds intervalInt64
+)
+{
+    // Set Timer
+    while (WM_QUIT != ParamStruct.msg.message)
+    {
+        callback(ParamStruct);
+        std::this_thread::sleep_for(intervalInt64);
+    }
+    ParamStruct.app.Cleanup();
+}
+
+void RenderCallback(ParamSTRUCT& ParamStruct)
+{
+    if (PeekMessage(&ParamStruct.msg, NULL, 0, 0, PM_REMOVE))
+    {
+        TranslateMessage(&ParamStruct.msg);
+        DispatchMessage(&ParamStruct.msg);
+    }
+
+    ParamStruct.app.Update();
+    ParamStruct.app.Render();
+}
 
 /**
  * Program entry point.
@@ -13,31 +52,31 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
     HRESULT hr = EXIT_SUCCESS;
     {
-        MSG msg = { 0 };
-
-        // Get the application configuration
-        ConfigInfo config;
-        hr = Utils::ParseCommandLine(lpCmdLine, config);
-        if (hr != EXIT_SUCCESS) return hr;
-
-        // Initialize
-        DXRApplication app;
-        app.Init(config);
-
-        // Main loop
-        while (WM_QUIT != msg.message)
+        try
         {
-            if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-            {
-                TranslateMessage(&msg);
-                DispatchMessage(&msg);
-            }
+            MSG msg = { 0 };
 
-            app.Update();
-            app.Render();
+            // Get the application configuration
+            ConfigInfo config;
+            hr = Utils::ParseCommandLine(lpCmdLine, config);
+            if (hr != EXIT_SUCCESS) return hr;
+
+            // Initialize
+            ParamSTRUCT ParamStruct;
+            ParamStruct.msg = msg;
+            ParamStruct.app.Init(config);
+
+            // Main loop
+            set_interval(
+                RenderCallback,
+                ParamStruct,
+                std::chrono::milliseconds(1000 / 2000) // eg. 1000/60 = 60fps
+            );
         }
-
-        app.Cleanup();
+        catch (std::invalid_argument& e)
+        {
+            std::cout << e.what() << std::endl;
+        }
     }
 
 #if defined _CRTDBG_MAP_ALLOC
